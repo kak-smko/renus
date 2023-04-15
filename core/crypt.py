8c7bcd88e925a84f44dccceaac0d53c69e081851
smko <ba.smko@gmail.com>
2023-04-15 12:46:10 +0330

    fix some bugs on shift and unshift encryption

--- core/crypt.py
+++ core/crypt.py
@@ -40,39 +40,30 @@ def decrypt(enc_dict:str, password:str)->str:
     return d.decode()
 
 
-def shift(text: str, password: str):
-    text += password
+def shift(clear, key):
+    clear += key
     rnd = get_random_string(6)
-    password = rnd + password
-    password = urlsafe_b64encode(password.encode()).decode()
-    text = urlsafe_b64encode(text.encode()).decode()
-    ls = []
-    ln = len(password)
-    for i in range(len(text)):
-        key_c = password[i % ln]
-        ls.append(chr((ord(text[i]) + ord(key_c)) % 256))
+    clear = rnd + clear
+    clear = urlsafe_b64encode(clear.encode()).decode()
+    enc = []
+    lnk=len(key)
+    for i in range(len(clear)):
+        key_c = key[i % lnk]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
 
-    text = rnd + ''.join(ls)
-    text = urlsafe_b64encode(text.encode()).decode()
-    return remove_pad64(text)
+    return remove_pad64(urlsafe_b64encode("".join(enc).encode()).decode())
 
 
-def unshift(text: str, password: str):
+def unshift(enc, key):
     try:
-        lnpass = len(password)
-        text = add_pad64(text)
-        text = urlsafe_b64decode(text).decode()
-        rnd = text[:6]
-        text = text[6:]
-        password = rnd + password
-        password = urlsafe_b64encode(password.encode()).decode()
-        ls = []
-        ln = len(password)
-        for i in range(len(text)):
-            key_c = password[i % ln]
-            ls.append(chr((256 + ord(text[i]) - ord(key_c)) % 256))
-
-        text = ''.join(ls).rstrip()
-        return urlsafe_b64decode(text).decode()[:-lnpass]
+        enc = urlsafe_b64decode(add_pad64(enc)).decode()
+        ln = len(key)
+        dec = []
+        for i in range(len(enc)):
+            key_c = key[i % ln]
+            dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+            dec.append(dec_c)
+        return urlsafe_b64decode("".join(dec)).decode()[6:-ln]
     except:
         raise RuntimeError('Invalid Token Format')
